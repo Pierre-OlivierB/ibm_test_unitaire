d365113e1eac75e5e16992722f457d18e7a9d750
unknown <baudinopierreolivier@gmail.com>
2023-07-20 12:07:30 +0200

    add base configuration for test unit

---
+++ fun.test.js
@@ -0,0 +1,11 @@
+import sum from "./fun";
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+test("adds 1 + 2 to not equal 0", () => {
+  expect(sum(1, 2)).not.toBe(0);
+});
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(0);
+});
